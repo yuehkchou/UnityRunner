ca50711043cb3961f7e35c6335ea7d8928161b6e
Yueh Chou <yueh.k.chou@gmail.com>
2017-04-12 21:27:53 -0400

    init() initial Runner Class setting

---
+++ Scripts/Runner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Runner : MonoBehaviour {
+
+	private CharacterController controller;
+
+	public float speed = 3.0f;
+
+	// Intialization
+	void Start () {
+		controller = GetComponent<CharacterController>();
+	}
+
+	// Update per frame
+	void Update () {
+		controller.Move((Vector3.forward * speed) * Time.deltaTime);
+
+	}
+}  
