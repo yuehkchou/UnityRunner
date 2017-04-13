da8b52152a60e8277711fcbdbc6d10ad9b2b2395
Yueh Chou <yueh.k.chou@gmail.com>
2017-04-12 22:04:04 -0400

    feat() player gravity/vertical position change

--- Scripts/Runner.cs
+++ Scripts/Runner.cs
@@ -1,11 +1,15 @@
-﻿using UnityEngine;
+using UnityEngine;
+using System.Collections;
 
 public class Runner : MonoBehaviour {
 
 	private Vector3 directionVector;
 	private CharacterController controller;
 
+	// Initial Values
 	public float speed = 3.0f;
+	public float verticalVelocity = 0.0f;
+	public float gravity = 9.8f;
 
 	// Intialization
 	void Start () {
@@ -16,11 +20,20 @@ public class Runner : MonoBehaviour {
 	void Update () {
 		directionVector = Vector3.zero;
 
+		//
+		if (controller.isGrounded) {
+			verticalVelocity = -0.5f;
+		} else {
+			verticalVelocity -= gravity * Time.deltaTime;
+		}
 		// X - Left or Right
-		directionVector.x = Input.GetAxis("Horizontal");
+		directionVector.x = Input.GetAxis("Horizontal") * speed;
 
 		// Y - Up and Down
+		directionVector.y = verticalVelocity;
+
 		// Z - Forward and Backward
+		directionVector.z = speed;
 		controller.Move((directionVector * speed) * Time.deltaTime);
 	}
 }
