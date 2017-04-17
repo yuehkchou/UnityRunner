9c6ccd63b12c960d7bbba7d7ff7dc9c7d60b44de
Yueh Chou <yueh.k.chou@gmail.com>
2017-04-17 17:21:45 -0400

    feat() added score calculator for level difficulty increase and onDeath

--- Scripts/Score.cs
+++ Scripts/Score.cs
@@ -1,16 +1,42 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Score : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    private float score = 0.0f;
+
+    public Text scoreText;
+    private float difficultyLevel = 1;
+    private float maxDifficultyLevel = 10;
+    private float scoreToNextLevel = 10;
+
+    private bool isDead = false;
+
+    // Use this for initialization
+    void Start () {
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
+        if(isDead) {
+            return;
+        }
+        score += Time.deltaTime;
+        if (score >= scoreToNextLevel)
+            LevelUp();
+        scoreText.text = ((int)score).ToString();
 	}
+    void LevelUp() {
+        scoreToNextLevel *= 2;
+        difficultyLevel++;
+        GetComponent<Runner>().SetSpeed(difficultyLevel);
+    }
+
+    public void onDeath() {
+        isDead = true;
+    }
 }
