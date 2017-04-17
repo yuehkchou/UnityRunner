4f3e82177532c06271c9b9e2eb583dbaf27edbe1
Yueh Chou <yueh.k.chou@gmail.com>
2017-04-17 12:30:27 -0400

    feat() tiles now destroy after a certain distance traveled

--- Scripts/TileGenerator.cs
+++ Scripts/TileGenerator.cs
@@ -1,42 +1,59 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TileGenerator : MonoBehaviour {
 
-	private Transform playerTransform;
+    // Array for injecting new Prefabs
+    public GameObject[] tilePrefabs;
 
-	// Z value where the tile spawn
-	private float spawnZ= 0.0f;
+    private Transform playerTransform;
+
+    // Z value where the tile spawn
+    private float spawnZ= -40.0f;
 
 	// Length of tile on Screen
-	private float tileLength = 8.0f;
+	private float tileLength = 11.0f;
 
 	// Number of Tiles on screen
-	private int numTileOnScreen = 4;
+	private int numTilesOnScreen = 4;
 
-	public GameObject[] tilePrefabs;
+    // Creating a zone of
+    private float safeZone = 25.0f;
+
+    // Store Tileset
+    private List<GameObject> activeTiles;
+
+
 	// Use this for initialization
 	void Start () {
+        activeTiles = new List<GameObject>();
 		playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
 
-		for(int i = 0; i < numTileOnScreen; i++) {
-			SpawnTile();
-		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(playerTransform.position.z > (spawnZ - numTileOnScreen * tileLength)) {
+		for(int i = 0; i < numTilesOnScreen; i++) {
 			SpawnTile();
 		}
 	}
 
-	void SpawnTile(int prefabInd = -1) {
+	// Update is called once per frame
+	private void Update () {
+        if (playerTransform.position.z - safeZone > (spawnZ - numTilesOnScreen * tileLength)) {
+			SpawnTile();
+            RemoveTile();
+		}
+	}
+
+	private void SpawnTile(int prefabInd = -1) {
 		GameObject tileset;
 		tileset = Instantiate(tilePrefabs[0]) as GameObject;
-		tileset.transform.SetParent(this.transform);
+		tileset.transform.SetParent(transform);
 		tileset.transform.position = Vector3.forward * spawnZ;
 		spawnZ += tileLength;
+        activeTiles.Add(tileset);
+	}
+
+	private void RemoveTile() {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
 	}
 }
