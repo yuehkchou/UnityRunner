c0d5e2019af81d9b9d3c466ce4c3944e328f8505
Yueh Chou <yueh.k.chou@gmail.com>
2017-04-17 21:06:29 -0400

    feat() deathmenu.toggle transition settings

--- Scripts/DeathMenu.cs
+++ Scripts/DeathMenu.cs
@@ -1,16 +1,42 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class DeathMenu : MonoBehaviour {
 
+
+    public Text scoreText;
+    public Image backgroundImg;
+
+    private bool isShowned = false;
+
+    private float transition = 0.0f;
+
 	// Use this for initialization
 	void Start () {
-		
+        gameObject.SetActive(false);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+        if (!isShowned)
+            return;
+        transition += Time.deltaTime;
+        backgroundImg.color = Color.Lerp(new Color(0, 0, 0, 0), Color.black, transition);
 	}
+
+    public void ToggleEndMenu(float score) {
+        gameObject.SetActive(true);
+        scoreText.text = ((int)score).ToString();
+        isShowned = true;
+    }
+
+    public void Restart() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void ToMenu() {
+        SceneManager.LoadScene("Menu");
+    }
 }
