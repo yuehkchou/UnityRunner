adb2972f3f9ea8cc03d1a2f822a2a4f259195afe
Yueh Chou <yueh.k.chou@gmail.com>
2017-04-17 23:32:56 -0400

    feat() display high score.

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+public Text highscoreText;
+
 	// Use this for initialization
 	void Start () {
-
+    highscoreText.text = "Highscore :" + PlayerPrefs.GetFloat("Highscore");
 	}
 
 	// Update is called once per frame
 	void Update () {
-
 	}
 
-    public void ToGame() {
-        SceneManager.LoadScene("EndlessRun");
-    }
+  public void ToGame() {
+      SceneManager.LoadScene("EndlessRun");
+  }
 }
